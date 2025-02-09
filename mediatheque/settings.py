c5cac24c7fa55d20a62c901b67d5ccf82f97fd5c
laurac <laurac0362@gmail.com>
2025-02-09 10:38:10 +0100

    add favicon

--- mediatheque/settings.py
+++ mediatheque/settings.py
@@ -85,6 +85,11 @@ DATABASES = {
     }
 }
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
